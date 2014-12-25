7ecd14bed3f351f27ae71a15c56b1290c112ed6e
Eiji Takahashi <tak7iji@gmail.com>
2014-12-25 16:54:58 +0900

    SpringのTransactionManagerを利用する

--- src/main/java/org/sample/springmvc/config/AppConfig.java
+++ src/main/java/org/sample/springmvc/config/AppConfig.java
@@ -1,24 +1,35 @@
 package org.sample.springmvc.config;
 
-import java.io.InputStream;
+import java.util.Properties;
 
-import org.apache.ibatis.io.Resources;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.managed.ManagedTransactionFactory;
+import org.sample.mybatis.SqlMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 @ComponentScan(basePackages="org.sample")
-public class AppConfig extends WebMvcConfigurationSupport {
-
+public class AppConfig {
     @Bean
     public ViewResolver setupViewResolver() {
         UrlBasedViewResolver resolver = new UrlBasedViewResolver(); 
@@ -34,14 +45,55 @@ public class AppConfig extends WebMvcConfigurationSupport {
         System.out.println("Set SqlSessionFactory");
         
         try {
-            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            DataSource dataSource = dataSource();
+            // 設定ファイルを使わずにSqlSessionFactoryを作成
+
+            TransactionFactory transactionFactory = new ManagedTransactionFactory();
+            Environment environment = new Environment("development", transactionFactory, dataSource);
+            org.apache.ibatis.session.Configuration conf = new org.apache.ibatis.session.Configuration(environment);
+
+            // DatabaseMetaData#getDatabaseProductName()から返された文字列が下記プロパティキーの文字列を含む場合に、
+            // プロパティ値をDatabaseIdとして設定する
+            Properties props = new Properties();
+            props.setProperty("Oracle", "oracle");
+            props.setProperty("PostgreSQL", "postgresql");
+            props.setProperty("MySQL", "mysql");
+            VendorDatabaseIdProvider provider = new VendorDatabaseIdProvider();
+            provider.setProperties(props);
+            conf.setDatabaseId(provider.getDatabaseId(dataSource));
             
+            // SqlMapperのpackageからSqlMapper.xmlを検索して読み込み（するらしい）
+            conf.addMapper(SqlMapper.class);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(conf);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         System.out.println(sqlSessionFactory);
 
         return sqlSessionFactory;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        System.out.println("Set Datasource");
+        DataSource ds = null;
+        try {
+            // InitialContextを使っても大して変わらない？
+            JndiTemplate jndi = new JndiTemplate();
+            ds = (DataSource) jndi.lookup("java:comp/env/jdbc/tpcwDS");
+        } catch (Exception ex) {}
+        
+        // DataSourceをTransactionAwareDataSourceProxyでラップして返却
+        return new TransactionAwareDataSourceProxy(ds);
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        System.out.println("Set TransactionManager");
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        
+        dataSourceTransactionManager.setDataSource(dataSource());
+ 
+        return dataSourceTransactionManager;
     }
 }
--- src/main/java/org/sample/springmvc/extra/MultiRecord.java
+++ src/main/java/org/sample/springmvc/extra/MultiRecord.java
@@ -1,34 +1,22 @@
 package org.sample.springmvc.extra;
 
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.sample.mybatis.SqlMapper;
-import org.sample.mybatis.TestTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MultiRecord {
-    @Autowired SqlSessionFactory sqlSessionFactory;
+    @Autowired DBAccess dbAccess;
     
     @RequestMapping("/list")
-    public ModelAndView execute() {
+    public ModelAndView execute() throws Exception {
         
         ModelAndView mav = new ModelAndView("list");
-        
-        // 引数なしのopenSession()はauto-commitがfalse
-        SqlSession session = sqlSessionFactory.openSession();
-        System.out.println(session.getConfiguration().getDatabaseId());
-        
-        SqlMapper mapper = session.getMapper(SqlMapper.class);
-        
-        List<TestTable> list = mapper.selectTestTables(1, "false", "a");
-        mav.addObject("data", list);
+        try {
+            mav.addObject("data", dbAccess.dbAccess());
+        } finally {
+        }
         
         return mav;
     }
